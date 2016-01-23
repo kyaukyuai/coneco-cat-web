3d8049a39b6cea85b4d3ecf897c1e58dbd9fff0f
Yuya Kakui <mail@kakuiyuya.com>
2016-01-24 01:11:50 +0900

    improve button

--- public/assets/application-052ce34baf475757b9d215df5c21b31c08460faf206687ddc70d578da8f6e3e4.js
+++ public/assets/application-052ce34baf475757b9d215df5c21b31c08460faf206687ddc70d578da8f6e3e4.js
@@ -13610,7 +13610,8 @@ var shuffle_array = function(array) {
   return array;
 };
 $(function() {
-  $("button").click(function() {
+  $(".img-favorite").click(function() {
+    $(this).addClass("animated fadeOutUp");
     var that = this;
     var cat_id = $(this).val();
     var favorite_url = "/api/v1/favorites";
@@ -13621,35 +13622,54 @@ $(function() {
       data: {cat_id: cat_id},
       timeout: 10000,
       success: function() {
-        $(that).prop('disabled', true);
-        $(that).html("ふぁぼった");
-        $(that).toggleClass("btn-favorite");
       },
       error: function() {
       }
     });
   });
-});
-$(document).bind("page:load", function() {
-  $("button").click(function() {
-    var that = this;
-    var cat_id = $(this).val();
-    var favorite_url = "/api/v1/favorites";
-    console.log("start");
-    $.ajax({
-      url: favorite_url,
-      type: 'POST',
-      dataType: 'jsonp',
-      data: {cat_id: cat_id},
-      timeout: 10000,
-      success: function() {
-        $(that).prop('disabled', true);
-        $(that).html("ふぁぼった");
-        $(that).toggleClass("btn-favorite");
-      },
-      error: function() {
-      }
-    });
+  $(".img-good").on({
+    "click":function(){
+      $(this).addClass("animated bounce");
+      var that = this;
+      var cat_id = $(this).val();
+      var good_url = "/api/v1/events/good";
+      $.ajax({
+        url: good_url,
+        type: 'POST',
+        dataType: 'jsonp',
+        data: {cat_id: cat_id},
+        timeout: 10000,
+        success: function() {
+        },
+        error: function() {
+        }
+      });
+    },
+    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend":function(){
+      $(this).removeClass("animated bounce");
+    }
+  });
+  $(".img-bad").on({
+    "click":function(){
+      $(this).addClass("animated bounce");
+      var that = this;
+      var cat_id = $(this).val();
+      var bad_url = "/api/v1/events/bad";
+      $.ajax({
+        url: bad_url,
+        type: 'POST',
+        dataType: 'jsonp',
+        data: {cat_id: cat_id},
+        timeout: 10000,
+        success: function() {
+        },
+        error: function() {
+        }
+      });
+    },
+    "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend":function(){
+      $(this).removeClass("animated bounce");
+    }
   });
 });
 var get_flickr_pics = function() {
