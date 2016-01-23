60a095fc57beedaa2b20b9e3aa50b2df5ace4ad9
Yuya Kakui <mail@kakuiyuya.com>
2016-01-23 23:12:09 +0900

    up

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -4,14 +4,26 @@ class RankingController < ApplicationController
     authenticate_user
     selected_date = Date.today
     @fav_cats = InstagramCat.without_soft_destroyed.order(fav_count: :desc).where(:posted_at => selected_date.beginning_of_day..selected_date.end_of_day).page(1).per(30)
-    @profile = @current_user.image if user_signed_in?
+    if user_signed_in?
+      @profile = @current_user.image
+      @fav_flg = {}
+      @fav_cats.each do |fav_cat|
+        @fav_flg[fav_cat[:id]] = Favorite.exists?(:cat_id => fav_cat[:id], :user_id => @current_user.id)
+      end
+    end
   end
 
   def daily
     authenticate_user
     selected_date = Date.today
     @fav_cats = InstagramCat.without_soft_destroyed.order(fav_count: :desc).where(:posted_at => selected_date.beginning_of_day..selected_date.end_of_day).page(1).per(30)
-    @profile = @current_user.image if user_signed_in?
+    if user_signed_in?
+      @profile = @current_user.image
+      @fav_flg = {}
+      @fav_cats.each do |fav_cat|
+        @fav_flg[fav_cat[:id]] = Favorite.exists?(:cat_id => fav_cat[:id], :user_id => @current_user.id)
+      end
+    end
   end
 
   def monthly
@@ -21,14 +33,26 @@ class RankingController < ApplicationController
     end_date = start_date >> 1
     end_date = end_date - 1
     @fav_cats = InstagramCat.without_soft_destroyed.order(fav_count: :desc).where(:posted_at => start_date..end_date).page(1).per(30)
-    @profile = @current_user.image if user_signed_in?
+    if user_signed_in?
+      @profile = @current_user.image
+      @fav_flg = {}
+      @fav_cats.each do |fav_cat|
+        @fav_flg[fav_cat[:id]] = Favorite.exists?(:cat_id => fav_cat[:id], :user_id => @current_user.id)
+      end
+    end
   end
 
   def total
     authenticate_user
     @selected_date = Date.today
     @fav_cats = InstagramCat.without_soft_destroyed.order(fav_count: :desc).page(1).per(30)
-    @profile = @current_user.image if user_signed_in?
+    if user_signed_in?
+      @profile = @current_user.image
+      @fav_flg = {}
+      @fav_cats.each do |fav_cat|
+        @fav_flg[fav_cat[:id]] = Favorite.exists?(:cat_id => fav_cat[:id], :user_id => @current_user.id)
+      end
+    end
   end
 
 end
