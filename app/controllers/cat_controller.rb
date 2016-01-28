aafaf63b50b1fb7d1ac0750b921d0f32bdb99006
Yuya Kakui <mail@kakuiyuya.com>
2016-01-28 22:34:06 +0900

    up

--- app/controllers/cat_controller.rb
+++ app/controllers/cat_controller.rb
@@ -10,5 +10,11 @@ class CatController < ApplicationController
       @fav_flg = {}
       @fav_flg[@cat[:id]] = Favorite.exists?(:cat_id => @cat[:id], :user_id => @current_user.id)
     end
+    page = [*1..10].sample.to_i
+    @cats = InstagramCat
+              .without_soft_destroyed
+              .order(fav_count: :desc)
+              .page(page)
+              .per(6)
   end
 end
